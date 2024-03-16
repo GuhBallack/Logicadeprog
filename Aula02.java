949293ee94e0b61ac87aac7430c20f7bc990e896
GuhBallack <163609702+GuhBallack@users.noreply.github.com>
2024-03-15 21:43:32 -0300

    Create Aula02.java

---
+++ Aula02.java
@@ -0,0 +1,16 @@
+package Aula02;
+
+public class Atividade03 {
+
+	public static void main(String[] args) {
+		String  nome = "Gustavo";
+		String cpf = "107.550.919-06";
+		int cep = 31050230;
+		String cidade = "Curitiba";
+		String estado = "Parana";
+		
+		System.out.println("Nome da pessoa " + nome + ", possui o cpf " + cpf + ", residente no cep " + cep + ", na cidade " + cidade + " estado do " + estado);
+
+	}
+
+}
